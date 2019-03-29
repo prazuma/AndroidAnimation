d083b68391e728d851a56685b917b6074934f1e5
hashimoto.mika <hashimoto.mika@moneyforward.co.jp>
2019-03-29 18:54:55 +0900

    Created ValueAnimator with custom type to animator.

--- app/src/main/java/com/prazuma/androidanimation/PropertyAnimationActivity.kt
+++ app/src/main/java/com/prazuma/androidanimation/PropertyAnimationActivity.kt
@@ -1,5 +1,6 @@
 package com.prazuma.androidanimation
 
+import android.animation.TypeEvaluator
 import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.View
@@ -15,6 +16,10 @@ class PropertyAnimationActivity : AppCompatActivity() {
         property_animation_value_button.setOnClickListener {
             doValueAnimation(property_animation_image)
         }
+
+        property_animation_value_with_type_evaluator_button.setOnClickListener {
+            doValueAnimatorWithTypeEvaluator(property_animation_image)
+        }
     }
 
     private fun doValueAnimation(view: View) {
@@ -24,6 +29,42 @@ class PropertyAnimationActivity : AppCompatActivity() {
                 view.translationX = updatedAnimation.animatedValue as Float
             }
             start()
+        }
+    }
+
+    private fun doValueAnimatorWithTypeEvaluator(view: View) {
+        ValueAnimator.ofObject(
+                MyTypeEvaluator(),
+                Pair(0f, 0f),
+                Pair(100f, 100f)
+        ).apply {
+            duration = 1000
+            addUpdateListener { updatedAnimation ->
+                val pair = updatedAnimation.animatedValue as Pair<*, *>
+                view.translationX = pair.first as Float
+                view.translationY = pair.second as Float
+            }
+            start()
+        }
+    }
+
+    class MyTypeEvaluator : TypeEvaluator<Pair<Float, Float>> {
+        override fun evaluate(
+                fraction: Float,
+                startValue: Pair<Float, Float>?,
+                endValue: Pair<Float, Float>?
+        ): Pair<Float, Float> {
+            startValue ?: return Pair(0f, 0f)
+            endValue ?: return Pair(0f, 0f)
+
+            return when {
+                fraction < 0.3f -> startValue
+                fraction < 0.6f -> {
+                    Pair((startValue.first + endValue.first) / 2,
+                            (startValue.second + endValue.second) / 2)
+                }
+                else -> endValue
+            }
         }
     }
 }
