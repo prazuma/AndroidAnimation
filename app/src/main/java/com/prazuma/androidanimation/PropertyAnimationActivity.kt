990462fb9ffff7ebc4ef2200768a17424bfc0b44
hashimoto.mika <hashimoto.mika@moneyforward.co.jp>
2019-04-01 19:14:35 +0900

    Created multiple animations using animator set

--- app/src/main/java/com/prazuma/androidanimation/PropertyAnimationActivity.kt
+++ app/src/main/java/com/prazuma/androidanimation/PropertyAnimationActivity.kt
@@ -1,5 +1,6 @@
 package com.prazuma.androidanimation
 
+import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.animation.TypeEvaluator
 import android.animation.ValueAnimator
@@ -28,6 +29,10 @@ class PropertyAnimationActivity : AppCompatActivity() {
 
         property_animation_object_with_type_evaluator.setOnClickListener {
             doObjectAnimatorWithTypeEvaluator(property_animation_image)
+        }
+
+        property_animation_multi_button.setOnClickListener {
+            doMultipleAnimationsUsingAnimatorSet(property_animation_image)
         }
     }
 
@@ -78,6 +83,35 @@ class PropertyAnimationActivity : AppCompatActivity() {
                 view.translationX = pair.first as Float
                 view.translationY = pair.second as Float
             }
+            start()
+        }
+    }
+
+    private fun doMultipleAnimationsUsingAnimatorSet(view: View) {
+        val upAnimation = ObjectAnimator.ofFloat(view, "translationY", -50f).apply {
+            duration = 250
+        }
+        val downAnimation = ObjectAnimator.ofFloat(view, "translationY", 50f).apply{
+            duration = 250
+        }
+        val scaleXAnimation1 = ObjectAnimator.ofFloat(view, "scaleX", 10f).apply {
+            duration = 100
+        }
+        val scaleYAnimation1 = ObjectAnimator.ofFloat(view, "scaleY", 10f).apply {
+            duration = 100
+        }
+        val scaleXAnimation2 = ObjectAnimator.ofFloat(view, "scaleX", 1f).apply {
+            duration = 100
+        }
+        val scaleYAnimation2 = ObjectAnimator.ofFloat(view, "scaleY", 1f).apply {
+            duration = 100
+        }
+        AnimatorSet().apply {
+            play(upAnimation).before(scaleXAnimation1)
+            play(scaleXAnimation1).with(scaleYAnimation1)
+            play(scaleXAnimation2).after(scaleYAnimation1)
+            play(scaleXAnimation2).with(scaleYAnimation2)
+            play(downAnimation).after(scaleXAnimation2)
             start()
         }
     }
